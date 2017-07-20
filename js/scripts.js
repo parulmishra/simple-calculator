9655076ef732c3c7e64a7d5a9a948840eb1da55c
Parul Mishra <parul.mishra78@gmail.com>
2017-07-19 20:42:44 -0700

    this commit will add a working js and jquery scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,68 @@
 //business logic
 var firstNumber = "";
 var secondNumber = "";
+var operator = "";
+var operatorSelected = false;
+var result = 0;
 
+function add(num1, num2)
+{
+	return num1+num2;
+}
+function sub(num1, num2)
+{
+	return num1-num2;
+}
+function mul(num1, num2)
+{
+	return num1*num2;
+}
+function div(num1, num2)
+{
+	return num1/num2;
+}
 //front end logic
 $(document).ready(function(){
 
-$(".number").click(function(){
-  var number = $(this).val().toString();
-  firstNumber += number;
-  console.log(firstNumber);
-})
+	$(".number").click(function(){
+	var number = $(this).val().toString();
+	if(operatorSelected === false)
+	{
+		firstNumber += number;
+		$("#result").html(firstNumber);
+		$("#history").html(firstNumber);
+		//console.log(firstNumber);
+	}
+    else
+    {
+		secondNumber += number;
+		$("#result").html(secondNumber);
+		$("#history").html(firstNumber + " " + operator + " " + secondNumber);
+		//console.log(secondNumber);
+    }
+	});
+
+		$(".operation").click(function(){
+		operator = $(this).val().toString();
+		$("#history").html(firstNumber + " " + operator);
+		operatorSelected = true;
+		//console.log(operator);
+});
+
+	$(".equals").click(function(){
+		console.log(parseInt(firstNumber));
+		console.log(parseInt(secondNumber));
+		console.log(operator);
+		if(operator === "ADD")
+		result = add(parseInt(firstNumber), parseInt(secondNumber));
+		else if(operator === "SUB")
+		result = sub(parseInt(firstNumber), parseInt(secondNumber)); 
+		else if(operator === "MUL")
+		result = mul(parseInt(firstNumber), parseInt(secondNumber)); 
+		else if(operator === "DIV")
+		result = div(parseInt(firstNumber), parseInt(secondNumber)); 
+		//console.log(result);
+		$("#result").html(result);
+  });
+
 });
